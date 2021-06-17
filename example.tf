ce5653683b7f0ba06704ee43c1726b3277df9977
John Parr <parrjw@hotmail.com>
2021-06-17 10:21:12 +0200

    Create example.tf

---
+++ example.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "remote" {
+    organization = "parrsoftware"
+    workspaces {
+      name = "terraform-parrsoftware"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "northeurope"
+}
+
+resource "null_resource" "example" {
+        triggers = {
+          value = "A example resource that does nothing!"
+        }
+}
